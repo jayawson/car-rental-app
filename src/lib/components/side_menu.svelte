cf0444e2b33e01c3985fcf4585035756d59929d3
jerome <gabbidon3@yahoo.com>
2022-03-02 20:58:03 +0000

    working on side navigation

---
+++ src/lib/components/side_menu.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    // import MenuBar from "$lib/assets/svg/menu.svg"
+//export let active: boolean =true;
+
+</script>
+<nav class="h-full w-25 bg-slate-300 flex flex-col justify-between p-5 items-center">
+
+        <span >
+            <!-- <MenuBar/> -->
+            </span>
+        <span>2</span>
+        <span>3</span>
+    
+</nav>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,14 @@
 <script>
 	import '../app.css';
+	import SideMenu from '$lib/components/side_menu.svelte';
 </script>
 
-<slot />
+<div class=" flex items-stretch w-full h-full overflow-hidden">
+	<SideMenu />
+
+	<!-- <main class="bg-ghost-white-900  py-8 px-12 w-full overflow-y-auto">
+		<slot />
+	</main>
+
+	<aside class="bg-ghost-white-700  w-5/12 overflow-y-auto" /> -->
+</div>
